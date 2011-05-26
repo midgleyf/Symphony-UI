c60ce9a5b31d8ded2cd9c44b6766de8581b0d5e7
Frank Midgley <midgleyf@janelia.hhmi.org>
2011-05-26 16:46:49 +0000

    Fixed math error.

--- Utility/centerWindowOnScreen.m
+++ Utility/centerWindowOnScreen.m
@@ -2,6 +2,6 @@ function dims = centerWindowOnScreen(width, height)
     dpi = get(0, 'ScreenPixelsPerInch');
     sz = get(0, 'ScreenSize') / dpi * 72;
     x = sz(1) + sz(3) / 2 - width / 2;
-    y = sz(2) + sz(4) / 2 + height / 2;
+    y = sz(2) + sz(4) / 2 - height / 2;
     dims = uint16([x y width height]);
 end
