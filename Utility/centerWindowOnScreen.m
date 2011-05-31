c597a9fe65c679a85c901a30327299bf66b27302
Frank Midgley <midgleyf@janelia.hhmi.org>
2011-05-31 22:00:48 +0000

    Now using the 'MonitorPositions' property to better(?) handle multiple monitors.

--- Utility/centerWindowOnScreen.m
+++ Utility/centerWindowOnScreen.m
@@ -1,7 +1,14 @@
 function dims = centerWindowOnScreen(width, height)
+    % Returns the left, bottom, width and height to use to center a window of the given size on the main screen.
+    
     dpi = get(0, 'ScreenPixelsPerInch');
-    sz = get(0, 'ScreenSize') / dpi * 72;
+    
+    prevUnits = get(0, 'Units');
+    set(0, 'Units', 'pixels');
+    sz = get(0, 'MonitorPositions');
+    %sz = sz(1, :) / dpi * 72;
     x = sz(1) + sz(3) / 2 - width / 2;
     y = sz(2) + sz(4) / 2 - height / 2;
     dims = uint16([x y width height]);
+    set(0, 'Units', prevUnits);
 end
