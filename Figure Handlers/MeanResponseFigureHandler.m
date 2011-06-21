664116a02947660f77cd4c286ccd326972948ef0
Frank Midgley <midgleyf@janelia.hhmi.org>
2011-06-21 15:09:44 +0000

    The x-axis ticks are now correct if different classes of epoch have different lengths.

--- Figure Handlers/MeanResponseFigureHandler.m
+++ Figure Handlers/MeanResponseFigureHandler.m
@@ -5,7 +5,7 @@ classdef MeanResponseFigureHandler < FigureHandler
     end
     
     properties
-        meanPlots
+        meanPlots   % array of structures to store the properties of each class of epoch.
     end
     
     methods
@@ -17,11 +17,11 @@ classdef MeanResponseFigureHandler < FigureHandler
             
             obj.resetPlots();
         end
-
-
+        
+        
         function handleCurrentEpoch(obj)
             [responseData, sampleRate, units] = obj.protocolPlugin.response();
-
+            
             % Check if we have existing data for this "class" of epoch.
             % The class of the epoch is defined by the set of its unique parameters.
             epochParams = obj.protocolPlugin.epochSpecificParameters();
@@ -32,11 +32,11 @@ classdef MeanResponseFigureHandler < FigureHandler
                     break;
                 end
             end
-
+            
             if isempty(meanPlot)
                 % This is the first epoch of this class to be plotted.
                 meanPlot = {};
-                meanPlot.params = epochParams;  % The params that define this class of epochs.
+                meanPlot.params = epochParams;
                 meanPlot.data = responseData;
                 meanPlot.sampleRate = sampleRate;
                 meanPlot.units = units;
@@ -44,21 +44,26 @@ classdef MeanResponseFigureHandler < FigureHandler
                 hold(obj.axesHandle, 'on');
                 meanPlot.plotHandle = plot(obj.axesHandle, 1:numel(meanPlot.data), meanPlot.data);
                 obj.meanPlots(end + 1) = meanPlot;
-
-                duration = numel(meanPlot.data) / sampleRate;
-                samplesPerTenth = sampleRate / 10;
-                set(obj.axesHandle, 'XTick', 1:samplesPerTenth:numel(meanPlot.data), ...
-                                    'XTickLabel', 0:.1:duration); % TODO: there could be more samples in a later epoch...
-                ylabel(obj.axesHandle, units);
             else
                 % This class of epoch has been seen before, add the current response to the mean.
                 % TODO: Adjust response data to the same sample rate and unit as previous epochs if needed.
+                % TODO: if the length of data is varying then the mean will not be correct beyond the min length.
                 meanPlot.data = (meanPlot.data * meanPlot.count + responseData) / (meanPlot.count + 1);
                 meanPlot.count = meanPlot.count + 1;
                 set(meanPlot.plotHandle, 'XData', 1:numel(meanPlot.data), ...
                                          'YData', meanPlot.data);
                 obj.meanPlots(i) = meanPlot;
             end
+            
+            % Draw ticks every 0.1 seconds.
+            maxSamples = max(arrayfun(@(x) numel(x.data), obj.meanPlots));
+            duration = maxSamples / sampleRate;
+            samplesPerTenth = sampleRate / 10;
+            set(obj.axesHandle, 'XTick', 1:samplesPerTenth:maxSamples, ...
+                                'XTickLabel', 0:.1:duration);
+            
+            % Update the y axis with the units of the response.
+            ylabel(obj.axesHandle, units);
         end
         
         
@@ -70,7 +75,12 @@ classdef MeanResponseFigureHandler < FigureHandler
         
         
         function resetPlots(obj)
-            obj.meanPlots = struct('params', {}, 'data', {}, 'sampleRate', {}, 'units', {}, 'count', {}, 'plotHandle', {});
+            obj.meanPlots = struct('params', {}, ...        % The params that define this class of epochs.
+                                   'data', {}, ...          % The mean of all responses of this class.
+                                   'sampleRate', {}, ...    % The sampling rate of the mean response.
+                                   'units', {}, ...         % The units of the mean response.
+                                   'count', {}, ...         % The number of responses used to calculate the mean reponse.
+                                   'plotHandle', {});       % The handle of the plot for the mean response of this class.
         end
         
     end
