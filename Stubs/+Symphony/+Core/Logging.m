5ad5edb315b3701df251f91a8be367a96e23fbf5
Frank Midgley <midgleyf@janelia.hhmi.org>
2011-07-07 18:37:42 +0000

    Adding Symphony.Core.Logging stub.

---
+++ Stubs/+Symphony/+Core/Logging.m
@@ -0,0 +1,12 @@
+classdef Logging < handle
+   
+    properties
+        
+    end
+    
+    methods (Static)
+        function ConfigureLogging(configFilePath, outputPath) %#ok<INUSD>
+            
+        end
+    end
+end
