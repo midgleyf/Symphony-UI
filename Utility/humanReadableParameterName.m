fb2322d255bb99fa5e6f5b37a151712d13d36945
Frank Midgley <midgleyf@janelia.hhmi.org>
2011-08-17 17:00:46 +0000

    Now adding a space between consecutive upper case characters.

--- Utility/humanReadableParameterName.m
+++ Utility/humanReadableParameterName.m
@@ -1,5 +1,6 @@
 function hrn = humanReadableParameterName(n)
     hrn = regexprep(n, '([^A-Z])([A-Z])', '$1 $2');
+    hrn = regexprep(hrn, '([A-Z])([A-Z])', '$1 $2');
     hrn = strrep(hrn, '_', ' ');
     hrn(1) = upper(hrn(1));
 end
