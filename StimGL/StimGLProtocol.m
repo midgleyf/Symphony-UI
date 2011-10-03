5bcc1bc18d947e22aa3a215c9e6088886d241509
Sam Gale <gales@janelia.hhmi.org>
2011-10-03 16:29:47 +0000

    user protocol controls stimGL bgcolor and intertrialBg

--- StimGL/StimGLProtocol.m
+++ StimGL/StimGLProtocol.m
@@ -7,13 +7,12 @@ classdef StimGLProtocol < SymphonyProtocol
     properties (Hidden)
         stimGL
         loopCount
-        backgroundColor = [0.0 0.0 0.0]
     end
     
     properties
         % These properties are available to all StimGL plug-ins.
-        animationDuration = 10.0        % the number of seconds to run the plug-in
-        numberOfLoops = uint32(1)       % the number of times to run the plug-in
+        animationDuration = 1;       % the number of seconds to run the plug-in
+        numberOfLoops = uint32(1);       % the number of times to run the plug-in
     end
     
     
@@ -43,28 +42,6 @@ classdef StimGLProtocol < SymphonyProtocol
             
             % We _don't_ set nLoops because we handle that in Symphony via multiple epochs.
             params.nLoops = 1;
-            
-            % Set the background color used between epochs to be the same as what is used during epochs so that there is no flash.
-            % The color used during an epoch can only be grayscale.
-            params.interTrialBg = obj.backgroundColor;
-            params.bgcolor = obj.backgroundColor(1);
-        end
-        
-        
-        function setBackgroundColor(obj, color)
-            if ~isnumeric(color) || (numel(color) ~= 1 && numel(color) ~=3)
-                error('The color parameter must be a single number or a triplet of numbers, e.g. 0.5 or [0.5 0.5 0.5].')
-            end
-            
-            if numel(color) == 3
-                if color(1) ~= color(2) || color(2) ~= color(3)
-                    warning('Symphony:StimGLBackgroundColor', 'StimGL only supports grayscale background colors during epochs.')
-                end
-                
-                color = mean(color);
-            end
-            
-            obj.backgroundColor = [color color color];
         end
         
         
