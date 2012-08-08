7c73b368a8cc49466058df5fe802bf44e116e4dd
Mark Cafaro <mark.cafaro@gmail.com>
2012-08-08 13:28:09 -0700

    Improved human readable param names to recognize numbers and acronyms

--- Utility/humanReadableParameterName.m
+++ Utility/humanReadableParameterName.m
@@ -4,8 +4,13 @@
 %  http://license.janelia.org/license/jfrc_copyright_1_1.html
 
 function hrn = humanReadableParameterName(n)
-    hrn = regexprep(n, '([^A-Z])([A-Z])', '$1 $2');
-    hrn = regexprep(hrn, '([A-Z])([A-Z])', '$1 $2');
-    hrn = strrep(hrn, '_', ' ');
+    hrn = regexprep(n, '([A-Z][a-z]+)', ' $1');
+    hrn = regexprep(hrn, '([A-Z][A-Z]+)', ' $1');
+    hrn = regexprep(hrn, '([^A-Za-z ]+)', ' $1');
+    hrn = strtrim(hrn);
+    
+    % TODO: improve underscore handling, this really only works with lowercase underscored variables
+    hrn = strrep(hrn, '_', '');
+    
     hrn(1) = upper(hrn(1));
 end
